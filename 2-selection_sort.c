80d23353e5ad79f877de3c164cd552ca1a91bf9b
David Mebo <mebodave@gmail.com>
2023-01-22 13:20:27 -0800

    selection sort in ascending order and performance file

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in an array
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
+}
