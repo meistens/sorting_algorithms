c4f97cc681ae26fdbba2211127f6882145f33513
David Mebo <mebodave@gmail.com>
2023-01-22 14:04:31 -0800

    index to element (why ALX?)

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,7 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min, temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
