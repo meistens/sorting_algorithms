8ad56422af90e156c5be713f1178d3f04dd3bd04
David Mebo <mebodave@gmail.com>
2023-01-24 14:12:29 -0800

    hoare, efficiency file

--- sort.h
+++ sort.h
@@ -29,5 +29,8 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void quicksort_hoare(int *array, int begining, int end, size_t size);
+int partition_hoare(int *array, int begining, int end, size_t size);
 
 #endif
